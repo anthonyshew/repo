3ff86982e0ff8bc00b6dc0204dee9c5079b295b9
Anthony Shew <anthonyshew@gmail.com>
2025-07-13 21:49:24 -0600

    WIP 24686

--- apps/family-app/app/layout.tsx
+++ apps/family-app/app/layout.tsx
@@ -15,19 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "Team Shew",
 	description: "We are the best.",
-	icons: {
-		icon: [
-			{ url: "/favicon/favicon.ico" },
-			{ url: "/favicon/favicon.svg", type: "image/svg+xml" },
-			{ url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-			{
-				url: "/favicon/favicon-96x96.png",
-				sizes: "180x180",
-				type: "image/png",
-			},
-		],
-		apple: "/favicon/apple-touch-icon.png",
-	},
 };
 
 export default function RootLayout({
--- apps/family-app/app/manifest.ts
+++ apps/family-app/app/manifest.ts
@@ -9,28 +9,28 @@ export default function manifest(): MetadataRoute.Manifest {
 		display: "standalone",
 		background_color: "#000000",
 		theme_color: "#000000",
-		icons: [
-			{
-				src: "/favicon/favicon-96x96.png",
-				sizes: "96x96",
-				type: "image/png",
-			},
-			{
-				src: "/favicon/web-app-manifest-192x192.png",
-				sizes: "192x192",
-				type: "image/png",
-			},
-			{
-				src: "/favicon/web-app-manifest-512x512.png",
-				sizes: "512x512",
-				type: "image/png",
-			},
-			{
-				src: "/favicon/apple-touch-icon.png",
-				sizes: "180x180",
-				type: "image/png",
-				purpose: "maskable",
-			},
-		],
+		// icons: [
+		// 	{
+		// 		src: "/favicon/favicon-96x96.png",
+		// 		sizes: "96x96",
+		// 		type: "image/png",
+		// 	},
+		// 	{
+		// 		src: "/favicon/web-app-manifest-192x192.png",
+		// 		sizes: "192x192",
+		// 		type: "image/png",
+		// 	},
+		// 	{
+		// 		src: "/favicon/web-app-manifest-512x512.png",
+		// 		sizes: "512x512",
+		// 		type: "image/png",
+		// 	},
+		// 	{
+		// 		src: "/favicon/apple-touch-icon.png",
+		// 		sizes: "180x180",
+		// 		type: "image/png",
+		// 		purpose: "maskable",
+		// 	},
+		// ],
 	};
 }
