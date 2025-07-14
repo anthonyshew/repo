80d16e22da9dd71048add5de529a487a6caf48bf
Anthony Shew <anthonyshew@gmail.com>
2025-07-13 21:05:51 -0600

    WIP eb637

--- apps/family-app/app/manifest.ts
+++ apps/family-app/app/manifest.ts
@@ -11,13 +11,23 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: "#432dd7",
 		icons: [
 			{
-				src: "/heart-shoe.png",
+				src: "/favicon/favicon-96x96.png",
+				sizes: "96x96",
+				type: "image/png",
+			},
+			{
+				src: "/favicon/web-app-manifest-192x192.png",
 				sizes: "192x192",
 				type: "image/png",
 			},
 			{
-				src: "/heart-shoe.png",
+				src: "/favicon/web-app-manifest-512x512.png",
 				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/favicon/apple-touch-icon.png",
+				sizes: "180x180",
 				type: "image/png",
 			},
 		],
