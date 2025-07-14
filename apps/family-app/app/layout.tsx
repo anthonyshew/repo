eb0eb2b3035dd7d3edeef4f6a763f16141b0f975
Anthony Shew <anthonyshew@gmail.com>
2025-07-13 21:40:05 -0600

    WIP bc7ce

--- apps/family-app/app/layout.tsx
+++ apps/family-app/app/layout.tsx
@@ -20,6 +20,11 @@ export const metadata: Metadata = {
 			{ url: "/favicon/favicon.ico" },
 			{ url: "/favicon/favicon.svg", type: "image/svg+xml" },
 			{ url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+			{
+				url: "/favicon/favicon-96x96.png",
+				sizes: "180x180",
+				type: "image/png",
+			},
 		],
 		apple: "/favicon/apple-touch-icon.png",
 	},
