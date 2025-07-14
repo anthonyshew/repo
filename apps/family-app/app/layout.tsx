193d75fb78ccaee85460d194628273afda0713c3
Anthony Shew <anthonyshew@gmail.com>
2025-07-13 22:24:59 -0600

    WIP 0e02c

--- apps/family-app/app/layout.tsx
+++ apps/family-app/app/layout.tsx
@@ -16,10 +16,24 @@ export const metadata: Metadata = {
 	title: "Team Shew",
 	description: "We are the best.",
 	appleWebApp: {
+		title: "Team Shew",
 		capable: true,
 		statusBarStyle: "black-translucent",
-		title: "Team Shew",
 	},
+	icons: [
+		{ url: "/favicon.ico", rel: "shortcut icon" },
+		{ sizes: "48x48", url: "/icons/48.png", type: "image/png" },
+		{ sizes: "57x57", url: "/icons/57.png", rel: "apple-touch-icon" },
+		{ sizes: "60x60", url: "/icons/60.png", rel: "apple-touch-icon" },
+		{ sizes: "72x72", url: "/icons/72.png", rel: "apple-touch-icon" },
+		{ sizes: "76x76", url: "/icons/76.png", rel: "apple-touch-icon" },
+		{ sizes: "96x96", url: "/icons/96.png", rel: "apple-touch-icon" },
+		{ sizes: "114x114", url: "/icons/114.png", rel: "apple-touch-icon" },
+		{ sizes: "120x120", url: "/icons/120.png", rel: "apple-touch-icon" },
+		{ sizes: "152x152", url: "/icons/152.png", rel: "apple-touch-icon" },
+		{ sizes: "167x167", url: "/icons/167.png", rel: "apple-touch-icon" },
+		{ sizes: "180x180", url: "/icons/180.png", rel: "apple-touch-icon" },
+	],
 };
 
 export default function RootLayout({
