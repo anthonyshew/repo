dc8b7cdcd5cf23956473e402841dcdfe5c151ddd
Anthony Shew <anthony.shew@vercel.com>
2025-07-26 13:56:46 -0600

    More manifest stuff. (#135)

--- apps/family-app/app/layout.tsx
+++ apps/family-app/app/layout.tsx
@@ -13,16 +13,30 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "Shew Family App",
+	title: "Team Shew",
 	description: "We are the best.",
-	icons: {
-		icon: [
-			{ url: "/favicon/favicon.ico" },
-			{ url: "/favicon/favicon.svg", type: "image/svg+xml" },
-			{ url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-		],
-		apple: "/favicon/apple-touch-icon.png",
+	appleWebApp: {
+		title: "Team Shew",
+		capable: true,
+		statusBarStyle: "black-translucent",
 	},
+	icons: [
+		{ url: "/favicon.ico", rel: "shortcut icon" },
+		{ url: "/favicon.svg", type: "image/svg+xml" },
+		{ sizes: "48x48", url: "/icons/48.png", type: "image/png" },
+		{ sizes: "57x57", url: "/icons/57.png", rel: "apple-touch-icon" },
+		{ sizes: "60x60", url: "/icons/60.png", rel: "apple-touch-icon" },
+		{ sizes: "72x72", url: "/icons/72.png", rel: "apple-touch-icon" },
+		{ sizes: "76x76", url: "/icons/76.png", rel: "apple-touch-icon" },
+		{ sizes: "96x96", url: "/icons/96.png", rel: "apple-touch-icon" },
+		{ sizes: "114x114", url: "/icons/114.png", rel: "apple-touch-icon" },
+		{ sizes: "120x120", url: "/icons/120.png", rel: "apple-touch-icon" },
+		{ sizes: "152x152", url: "/icons/152.png", rel: "apple-touch-icon" },
+		{ sizes: "167x167", url: "/icons/167.png", rel: "apple-touch-icon" },
+		{ sizes: "180x180", url: "/icons/180.png", rel: "apple-touch-icon" },
+		{ sizes: "192x192", url: "/icons/192.png", rel: "apple-touch-icon" },
+		{ sizes: "512x512", url: "/icons/512.png", rel: "apple-touch-icon" },
+	],
 };
 
 export default function RootLayout({
--- apps/family-app/app/manifest.ts
+++ apps/family-app/app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
 	return {
-		name: "Shew Family App",
-		short_name: "Shew Family App",
+		name: "Team Shew",
+		short_name: "Team Shew",
 		description: "We are the best.",
 		start_url: "/",
 		display: "standalone",
@@ -11,23 +11,70 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: "#432dd7",
 		icons: [
 			{
-				src: "/favicon/favicon-96x96.png",
+				src: "/icons/48.png",
+				sizes: "48x48",
+				type: "image/png",
+			},
+			{
+				src: "/icons/57.png",
+				sizes: "57x57",
+				type: "image/png",
+			},
+			{
+				src: "/icons/60.png",
+				sizes: "60x60",
+				type: "image/png",
+			},
+			{
+				src: "/icons/72.png",
+				sizes: "72x72",
+				type: "image/png",
+			},
+			{
+				src: "/icons/76.png",
+				sizes: "76x76",
+				type: "image/png",
+			},
+			{
+				src: "/icons/96.png",
 				sizes: "96x96",
 				type: "image/png",
 			},
 			{
-				src: "/favicon/web-app-manifest-192x192.png",
+				src: "/icons/114.png",
+				sizes: "114x114",
+				type: "image/png",
+			},
+			{
+				src: "/icons/120.png",
+				sizes: "120x120",
+				type: "image/png",
+			},
+			{
+				src: "/icons/152.png",
+				sizes: "152x152",
+				type: "image/png",
+			},
+			{
+				src: "/icons/167.png",
+				sizes: "167x167",
+				type: "image/png",
+			},
+			{
+				src: "/icons/180.png",
+				sizes: "180x180",
+				type: "image/png",
+			},
+
+			{
+				src: "/icons/192.png",
 				sizes: "192x192",
 				type: "image/png",
 			},
+
 			{
-				src: "/favicon/web-app-manifest-512x512.png",
+				src: "/icons/512.png",
 				sizes: "512x512",
-				type: "image/png",
-			},
-			{
-				src: "/favicon/apple-touch-icon.png",
-				sizes: "180x180",
 				type: "image/png",
 			},
 		],
