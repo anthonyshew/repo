2e44187ea40d5e3094d43dcee1e8e07ce891c673
Anthony Shew <anthony.shew@vercel.com>
2025-07-27 08:28:16 -0600

    Set up shadcn/ui. (#148)
    About to be looking real good.

--- packages/ui/src/components/Avatar.tsx
+++ packages/ui/src/components/Avatar.tsx
@@ -1,52 +1,53 @@
 "use client";
 
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
-import * as React from "react";
-import { twMerge } from "tailwind-merge";
+import type * as React from "react";
 
-const Avatar = React.forwardRef<
-	React.ElementRef<typeof AvatarPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
-	<AvatarPrimitive.Root
-		className={twMerge(
-			"ui:relative ui:flex ui:h-10 ui:w-10 ui:shrink-0 ui:overflow-hidden ui:rounded-full",
-			className,
-		)}
-		ref={ref}
-		{...props}
-	/>
-));
-Avatar.displayName = AvatarPrimitive.Root.displayName;
+import { cn } from "../lib/utils";
 
-const AvatarImage = React.forwardRef<
-	React.ElementRef<typeof AvatarPrimitive.Image>,
-	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-	<AvatarPrimitive.Image
-		className={twMerge(
-			"ui:aspect-square ui:h-full ui:w-full ui:animate-reveal",
-			className,
-		)}
-		ref={ref}
-		{...props}
-	/>
-));
-AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+function Avatar({
+	className,
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+	return (
+		<AvatarPrimitive.Root
+			data-slot="avatar"
+			className={cn(
+				"ui:relative ui:flex ui:size-8 ui:shrink-0 ui:overflow-hidden ui:rounded-full",
+				className,
+			)}
+			{...props}
+		/>
+	);
+}
 
-const AvatarFallback = React.forwardRef<
-	React.ElementRef<typeof AvatarPrimitive.Fallback>,
-	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(({ className, ...props }, ref) => (
-	<AvatarPrimitive.Fallback
-		className={twMerge(
-			"ui:flex ui:h-full ui:w-full ui:items-center ui:justify-center ui:rounded-full",
-			className,
-		)}
-		ref={ref}
-		{...props}
-	/>
-));
-AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+function AvatarImage({
+	className,
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+	return (
+		<AvatarPrimitive.Image
+			data-slot="avatar-image"
+			className={cn("ui:aspect-square ui:size-full", className)}
+			{...props}
+		/>
+	);
+}
+
+function AvatarFallback({
+	className,
+	...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
+	return (
+		<AvatarPrimitive.Fallback
+			data-slot="avatar-fallback"
+			className={cn(
+				"ui:bg-muted ui:flex ui:size-full ui:items-center ui:justify-center ui:rounded-full",
+				className,
+			)}
+			{...props}
+		/>
+	);
+}
 
 export { Avatar, AvatarImage, AvatarFallback };
