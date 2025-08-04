9794f76a06bd705869412f8f3866a201e6d18ed1
Anthony Shew <anthonyshew@gmail.com>
2025-08-03 20:06:08 -0600

    WIP 127ca

--- apps/portfolio/app/(portfolio)/layout.tsx
+++ apps/portfolio/app/(portfolio)/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 		>
 			<body>
 				<ThemeWrapper>
-					<main className="relative flex flex-col flex-auto max-w-5xl min-h-screen px-6 pb-4 mx-auto sm:py-8 lg:py-20">
+					<main className="relative font-sans flex flex-col flex-auto max-w-5xl min-h-screen px-6 pb-4 mx-auto sm:py-8 lg:py-20">
 						<div className="absolute right-4 top-8 md:hidden">
 							<ThemeController />
 						</div>
