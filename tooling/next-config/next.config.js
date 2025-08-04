810c6f78614e459263139045b824ac1b603fa153
Anthony Shew <anthonyshew@gmail.com>
2025-08-03 20:02:26 -0600

    WIP 57453

--- tooling/next-config/next.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    // We do these in GitHub Actions checks so we don't do them here.
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
-};
