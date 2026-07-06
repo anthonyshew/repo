3b9adc7e0abc95dd1f16d48519e9c949682d6fe5
Anthony Shew <anthonyshew@gmail.com>
2026-07-05 21:32:11 -0600

    Refresh homepage bio and social links

--- apps/portfolio/app/(portfolio)/page.tsx
+++ apps/portfolio/app/(portfolio)/page.tsx
@@ -1,13 +1,10 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/Avatar";
 import { Discord } from "@repo/ui/Discord";
-import { GitHub, ThreadsApp, YouTube } from "@repo/ui/Icons";
+import { GitHub, YouTube } from "@repo/ui/Icons";
 import { Xitter } from "@repo/ui/Xitter";
 import type { Metadata } from "next";
-import Link from "next/link";
 import { tagline } from "#/app/constants";
 import { buildMeta } from "#/app/metadata";
-
-export const revalidate = 3600; // hourly
+import { LucideMail } from "lucide-react";
 
 export const generateMetadata = (): Metadata => {
 	return buildMeta({
@@ -19,25 +16,12 @@ export const generateMetadata = (): Metadata => {
 export default function Home() {
 	return (
 		<div>
-			<div className="flex flex-row justify-center gap-4 sm:gap-8">
-				<Avatar className="w-12 h-12 sm:w-20 sm:h-20">
-					<AvatarImage src="/images/me.jpg" />
-					<AvatarFallback />
-				</Avatar>
-				<div className="my-auto">
-					<h1 className="m-0 text-xl font-bold text-black dark:text-white sm:text-4xl">
-						Anthony Shew
-					</h1>
-					<p className="m-0 mt-1 font-mono text-xs text-pretty sm:text-lg dark:text-neutral-400">
-						{tagline}
-					</p>
-				</div>
-			</div>
 			<div className="max-w-2xl mx-auto mt-8 prose dark:prose-invert">
+				<p>Hey, I&apos;m Anthony.</p>
+
 				<p>
-					Hey, I&apos;m Anthony. I played professional baseball for six years
-					and now I write software. I'm currently at Vercel working on
-					Turborepo, where I get to lead the{" "}
+					I played pro baseball for six years. Now, I ship software. Today, I
+					lead{" "}
 					<a
 						href="https://turborepo.com"
 						rel="noopener noreferrer"
@@ -45,12 +29,25 @@ export default function Home() {
 					>
 						Turborepo
 					</a>{" "}
-					community. If you'd like to learn more about me personally, I wrote a
-					little about how I got here in{" "}
-					<Link href="/blog/my-developer-story">this blog post</Link>.
+					at{" "}
+					<a
+						href="https://vercel.com/home"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Vercel
+					</a>
+					.
 				</p>
+				<p>Some of my favorite things...</p>
+				<ul>
+					<li>People: My wife and sons</li>
+					<li>Sport: Hockey</li>
+					<li>Word: Volition</li>
+					<li>Book: Relentless by Tim S. Grover</li>
+					<li>Music: August Burns Red, Dance Gavin Dance, Protest the Hero</li>
+				</ul>
 
-				<hr className="mt-8 mb-4" />
 				<div className="flex flex-col items-center justify-center sm:flex-row sm:gap-8">
 					<a
 						className="flex flex-row gap-2 my-4 hover:underline"
@@ -58,7 +55,7 @@ export default function Home() {
 						rel="noopener noreferrer"
 						target="_blank"
 					>
-						<Xitter className="relative top-0.5 w-6 h-6" /> Xitter
+						<Xitter className="relative top-0.5 w-6 h-6" />
 					</a>
 					<a
 						className="flex flex-row gap-2 my-4 hover:underline"
@@ -66,15 +63,7 @@ export default function Home() {
 						rel="noopener noreferrer"
 						target="_blank"
 					>
-						<YouTube className="relative top-0.5 w-6 h-6" /> YouTube
-					</a>
-					<a
-						className="flex flex-row gap-2 my-4 hover:underline"
-						href="https://www.threads.net/@anthonyshew"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						<ThreadsApp className="relative top-0.5 w-6 h-6" /> Threads
+						<YouTube className="relative top-0.5 w-6 h-6" />
 					</a>
 					<a
 						className="flex flex-row gap-2 my-4 hover:underline"
@@ -82,7 +71,7 @@ export default function Home() {
 						rel="noopener noreferrer"
 						target="_blank"
 					>
-						<Discord className="relative top-0.5 w-6 h-6" /> Discord
+						<Discord className="relative top-0.5 w-6 h-6" />
 					</a>
 					<a
 						className="flex flex-row gap-2 my-4 hover:underline"
@@ -90,10 +79,18 @@ export default function Home() {
 						rel="noopener noreferrer"
 						target="_blank"
 					>
-						<GitHub className="relative top-0.5 w-6 h-6" /> Bugs
+						<GitHub className="relative top-0.5 w-6 h-6" />
 					</a>
 				</div>
-				<hr className="mt-4 mb-8" />
+
+				<div className="flex justify-center">
+					<a
+						className="flex flex-row gap-2 my-4 hover:underline"
+						href="mailto:anthony@shew.dev?subject=Consulting"
+					>
+						<LucideMail className="relative top-0.5 w-6 h-6" /> Consulting
+					</a>
+				</div>
 			</div>
 		</div>
 	);
